b66e9aadc7d928ef1928fd1608e27b193eddd5cc
ahmed <ahmedlearn01@gmail.com>
2021-06-24 22:42:08 -0700

    initial commit

--- js/details.js
+++ js/details.js
@@ -1,31 +1,3 @@
-/* const content = document.querySelector('.content');
-const delPost = document.querySelector('#delete');
-
-const id = new URLSearchParams(window.location.search).get('id');
-console.log(id);
-
-window.addEventListener('DOMContentLoaded',async(e)=>{
-    const URL = 'http://localhost:3000/posts/';
-    const res = await fetch(URL+id);
-    const post = await res.json();
-    content.innerHTML = `
-    <div class="post">
-    <h2>${post.title}</h2>
-    <small>${post.likes} likes</small>
-    <p>${post.body}</p>
- </div>
-    `
-})
-
-delPost.addEventListener('click', async (e)=>{
-    await fetch('http://localhost:3000/posts/'+id,{
-        method: 'DELETE'
-    })
-
-    window.location.replace('/index.html');
-    
-})
-*/
 const content = document.querySelector('.content');
 const delPost = document.querySelector('#delete');
 const id = new URLSearchParams(window.location.search).get('id');
--- js/index.js
+++ js/index.js
@@ -1,35 +1,3 @@
-/* const blogs = document.querySelector('.blogs');
-const form = document.querySelector('form#search');
-
-const renderPosts = async (term)=> {
-    let URL = 'http://localhost:3000/posts';
-
-    if(term){
-        URL+=`?q=${term}`;
-    }
-    const res = await fetch(URL);
-    const posts = await res.json();
-
-    let templete = "";
-    posts.forEach(post=>{
-        templete += `
-         <div class="posts">
-            <h2>${post.title}</h2>
-            <small>${post.likes} likes</small>
-            <p>${post.body.slice(0,200)}</p>
-            <a href="/details.html?id=${post.id}">Read more</a>
-         </div>
-        `;
-    });
-    blogs.innerHTML = templete;
-}
-window.addEventListener('DOMContentLoaded', ()=>{renderPosts()})
-
-form.addEventListener('submit',e=>{
-    e.preventDefault();
-    renderPosts(form.term.value);
-})
-*/
 
 const blogs = document.querySelector('.blogs');
 
